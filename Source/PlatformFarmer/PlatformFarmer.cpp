36a14edb1035a46b95574517f6702c56686eb9d0
Jakub Kaczor <j.kaczor06@gmail.com>
2026-02-12 19:17:05 +0100

    Initial commit

---
+++ Source/PlatformFarmer/PlatformFarmer.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PlatformFarmer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PlatformFarmer, "PlatformFarmer" );
